cc8f7fe827d888fb42d296c20522edef22cb4fc0
Richy Rich <bigmoneyrich@Richys-MacBook-Air.local>
2026-03-08 18:04:31 -0400

    Fix target column name

--- Housing_Price_Prediction_App.py
+++ Housing_Price_Prediction_App.py
@@ -7,7 +7,7 @@ from sklearn.linear_model import LinearRegression
 
 # load dataset
 data = pd.read_csv("Housing_data.csv")
-print(data.columns)
+st.write(data.columns)
 X = data.drop("Predicted Price", axis=1)
 y = data["Predicted Price"]
 
